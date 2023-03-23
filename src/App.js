63f2e5fc716730f1fa644165ed2cec231e2d7f36
oshiro17 <nonokapiano@icloud.com>
2023-03-23 14:22:44 +0900

    first_commit

--- src/App.js
+++ src/App.js
@@ -4,24 +4,90 @@ import js_file from './menus.json';
 
 const App = () => {
   const [menu, setMenu] = useState(js_file.menus[0]);
-  const [optionId, setOption_id] = useState('menu_1-select_2-option_1');
+  // const [optionId, setOption_id] = useState('menu_1-select_2-option_1');
+  // const [listInfo, setListInfo] = useState({ menu: "menu_1", list1: "アプリケーション詳細", list2: "解析プラン", list3: null });
 
-  const handleClick = (menu) => {
-    setMenu(menu);
+  const handleClick = () => {
   }
+  // const handleSelectChange = (optionId) => {
+  //   setOption_id(optionId);
+  //   if (optionId.includes('select_1')) {
+  //     console.log("include,select_1!!");
+  //     setListInfo({ ...listInfo, list1: optionId });
+  //   }
+  //   if (optionId.includes('select_2')) {
 
-  const handleSelectChange = (optionId) => {
-    setOption_id(optionId);
+  //     setListInfo({ ...listInfo, list2: optionId });
+  //   }
+  // }
+  // if (optionId === listInfo.list2) {
+  //   console.log("wow");
+  // } const clickOk = () => {
+  //   setListInfo({ ...listInfo, menu: menu });
+  // }
+
+  const buttonClick = (menu) => {
+    let obj = document.getElementById('select');
+    console.log(obj);
+    obj.selectedIndex = -1;
+    dropdowns(menu);
+    setMenu(menu);
   }
 
   const button = js_file.menus.map((menu) => (
     <li key={menu.id}>
-      <a href={() => false} id={menu.id} className="btn" onClick={() => handleClick(menu)}>
+      <a
+        href={() => false}
+        id={menu.id}
+        className="btn"
+        onClick={() => buttonClick(menu)}
+      >
         {menu.title}
       </a>
     </li>
   ));
 
+  const dropdowns = () => {
+    return (<div>
+      <div className='dropdown'>
+        <select onChange={() => list1Change()}>
+          <option>{menu.selects[0].title}</option>
+          {menu.selects[0].options.map((option) => (
+            <option>
+              {option.title}
+            </option>
+          ))}</select>
+      </div>
+      <div className='dropdown'>
+        <select id='select'
+          onChange={() => handleClick()}><option>{menu.selects[1].title}</option>
+          {menu.selects[1].options.map((option) => (
+            <option>
+              {option.title}
+            </option>
+          ))}</select>
+      </div>
+
+      <div className='dropdown'>
+        <select onChange={() => handleClick()}><option>{menu.selects[2].title}</option>
+          {menu.selects[2].options.map((option) => (
+            <option>
+              {option.title}
+            </option>
+          ))}</select>
+      </div></div>)
+  }
+  // const menus = js_file.menus;
+
+  // const button2 = () => {
+  //   return (
+  //     <a href={() => false} className="btn" onClick={() => handleClick()}>
+  //       OK
+  //     </a>
+  //   );
+  // }
+
+  // console.log(menu);
   return (
     <div>
       <div id="button">
@@ -29,10 +95,13 @@ const App = () => {
           {button}
         </ul>
       </div>
-      <div className="dropdown">
+      <div>
+        {dropdowns()}
+      </div>
+      {/* <div className="dropdown">
         {menu.selects.map((select) => {
           if (optionId !== "menu_2-select_2-option_2" && optionId !== "menu_1-select_2-option_2" && (select.title === "オプション解析" || select.title === "オプション解析2")) {
-          return(<div></div>);
+            return (<div></div>);
           }
           else {
             return (
@@ -48,9 +117,11 @@ const App = () => {
               </select>)
           }
         })}
-      </div>
+      </div> */}
+      <ul>
+        {/* {button2()} */}
+      </ul>
     </div>
   );
 }
-
 export default App;
